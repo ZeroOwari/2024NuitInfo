0250736c288dbc0d5d1284c2a7743111b389283f
Clément Magnier <118550445+ZeroOwari@users.noreply.github.com>
2024-12-05 20:47:13 +0000

    BUTTON

--- Accueil.js
+++ Accueil.js
@@ -1,3 +1,13 @@
 <script type="text/javascript">
+window.onload = function() {
+    const consequencesDiv = document.querySelector('.consequences');
+    const paragraphs = consequencesDiv.querySelectorAll('p');
 
+    paragraphs.forEach(paragraph => {
+        const button = document.createElement('button');
+        button.textContent = paragraph.textContent;
+        consequencesDiv.appendChild(button);
+        paragraph.remove();
+    });
+};
 </script>
